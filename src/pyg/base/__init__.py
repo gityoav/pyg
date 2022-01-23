92694e1f28ab0457418c5cfb8a0a54a995207d71
gityoav <30290427+gityoav@users.noreply.github.com>
2022-01-23 18:27:12 +0000

    making pyg an aggregator

--- src/pyg/base/__init__.py
+++ src/pyg/base/__init__.py
@@ -1,38 +1,39 @@
-from pyg.base._as_float import as_float
-from pyg.base._as_list import as_list, as_tuple, first, last, passthru, unique, is_rng
-from pyg.base._acell import acell, acell_load, acell_func, acell_go
-from pyg.base._cache import cache
-from pyg.base._cell import cell, cell_go, cell_item, cell_func, cell_load, cell_output, cell_clear, cell_inputs, GRAPH, UPDATED
-from pyg.base._cfg import mkdir, cfg_read, cfg_write, CFG
-from pyg.base._dates import dt,dt_bump, today, ymd, TMIN, TMAX, DAY, futcodes, dt2str, is_bump, nth_weekday_of_month
-from pyg.base._dag import get_DAG, add_edge, topological_sort, descendants, del_edge
-from pyg.base._decorators import kwargs_support, wrapper, try_value, try_back, try_nan, try_none, try_zero, try_false, try_true, try_list, timer
-from pyg.base._dict import Dict, items_to_tree, tree_items, tree_keys, tree_values, tree_setitem, tree_getitem, tree_get, tree_update, dict_invert
-from pyg.base._dictattr import dictattr, relabel
-from pyg.base._dictable import dictable, dict_concat, is_dictable
-from pyg.base._drange import date_range, drange, calendar, Calendar, clock, as_time
-from pyg.base._encode import encode, decode, bson2pd, pd2bson, as_primitive
-from pyg.base._eq import eq, in_
-from pyg.base._file import path_name, path_dirname, path_join, mkdir, read_csv
-from pyg.base._getitem import getitem, callitem, callattr
-from pyg.base._inspect import getargspec, getargs, getcallargs, call_with_callargs, argspec_defaults, argspec_required, argspec_update, argspec_add, kwargs2args
-from pyg.base._logger import logger, get_logger
-from pyg.base._loop import loop, loops, len0, pd2np, shape, loop_all
-from pyg.base._named_dict import named_dict
-from pyg.base._pandas import df_index, df_columns, df_reindex, np_reindex, df_concat, df_fillna, presync, df_sync, \
-    add_, sub_, mul_, div_, pow_, df_slice, df_unslice, nona
-from pyg.base._parquet import pd_to_parquet, pd_read_parquet
-from pyg.base._perdictable import join, perdictable
-from pyg.base._reducer import reducer, reducing
-from pyg.base._sort import sort, cmp, Cmp
-from pyg.base._tree import is_tree, tree_to_table
-from pyg.base._tree_repr import tree_repr
-from pyg.base._txt import alphabet, ALPHABET, f12, replace, lower, upper, proper, strip, split, capitalize, common_prefix, deprefix
-from pyg.base._types import is_pd, is_arr, is_int, is_float, is_num, is_bool, is_str, is_nan, \
-    is_none, is_dict, is_iterable, is_date, is_df, is_series, is_ts, is_list, is_tuple,\
-    is_pds, is_arrs, is_ints, is_floats, is_nums, is_len, is_bools, is_strs, is_nans, is_nones, is_dicts, is_zero_len, \
-    is_iterables, is_tss, nan2none, NoneType, is_dates, is_lists
-from pyg.base._ulist import ulist, rng
-from pyg.base._waiter import waiter, async_wrapper
-from pyg.base._zip import zipper, lens
+from pyg_base import * 
+#from pyg.base._as_float import as_float
+#from pyg.base._as_list import as_list, as_tuple, first, last, passthru, unique, is_rng
+#from pyg.base._acell import acell, acell_load, acell_func, acell_go
+#from pyg.base._cache import cache
+#from pyg.base._cell import cell, cell_go, cell_item, cell_func, cell_load, cell_output, cell_clear, cell_inputs, GRAPH, UPDATED
+#from pyg.base._cfg import mkdir, cfg_read, cfg_write, CFG
+#from pyg.base._dates import dt,dt_bump, today, ymd, TMIN, TMAX, DAY, futcodes, dt2str, is_bump, nth_weekday_of_month
+#from pyg.base._dag import get_DAG, add_edge, topological_sort, descendants, del_edge
+#from pyg.base._decorators import kwargs_support, wrapper, try_value, try_back, try_nan, try_none, try_zero, try_false, try_true, try_list, timer
+#from pyg.base._dict import Dict, items_to_tree, tree_items, tree_keys, tree_values, tree_setitem, tree_getitem, tree_get, tree_update, dict_invert
+#from pyg.base._dictattr import dictattr, relabel
+#from pyg.base._dictable import dictable, dict_concat, is_dictable
+#from pyg.base._drange import date_range, drange, calendar, Calendar, clock, as_time
+#from pyg.base._encode import encode, decode, bson2pd, pd2bson, as_primitive
+#from pyg.base._eq import eq, in_
+#from pyg.base._file import path_name, path_dirname, path_join, mkdir, read_csv
+#from pyg.base._getitem import getitem, callitem, callattr
+#from pyg.base._inspect import getargspec, getargs, getcallargs, call_with_callargs, argspec_defaults, argspec_required, argspec_update, argspec_add, kwargs2args
+#from pyg.base._logger import logger, get_logger
+#from pyg.base._loop import loop, loops, len0, pd2np, shape, loop_all
+#from pyg.base._named_dict import named_dict
+#from pyg.base._pandas import df_index, df_columns, df_reindex, np_reindex, df_concat, df_fillna, presync, df_sync, \
+#    add_, sub_, mul_, div_, pow_, df_slice, df_unslice, nona
+#from pyg.base._parquet import pd_to_parquet, pd_read_parquet
+#from pyg.base._perdictable import join, perdictable
+#from pyg.base._reducer import reducer, reducing
+#from pyg.base._sort import sort, cmp, Cmp
+#from pyg.base._tree import is_tree, tree_to_table
+#from pyg.base._tree_repr import tree_repr
+#from pyg.base._txt import alphabet, ALPHABET, f12, replace, lower, upper, proper, strip, split, capitalize, common_prefix, deprefix
+#from pyg.base._types import is_pd, is_arr, is_int, is_float, is_num, is_bool, is_str, is_nan, \
+#    is_none, is_dict, is_iterable, is_date, is_df, is_series, is_ts, is_list, is_tuple,\
+#    is_pds, is_arrs, is_ints, is_floats, is_nums, is_len, is_bools, is_strs, is_nans, is_nones, is_dicts, is_zero_len, \
+#    is_iterables, is_tss, nan2none, NoneType, is_dates, is_lists
+#from pyg.base._ulist import ulist, rng
+#from pyg.base._waiter import waiter, async_wrapper
+#from pyg.base._zip import zipper, lens
 
---
+++ src/pyg/npy/__init__.py
@@ -0,0 +1,1 @@
+from pyg_npy import *
