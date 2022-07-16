45f2369d506884b1d1376bfdd48f4feb7c193bc6
gityoav <30290427+gityoav@users.noreply.github.com>
2022-07-16 20:20:53 +0100

    support sql/mongo division

--- src/pyg/__init__.py
+++ src/pyg/__init__.py
@@ -4,7 +4,13 @@ from pyg.npy import *
 from pyg.base import *
 from pyg.bond import * 
 from pyg.encoders import * 
-from pyg.sql import *
-from pyg.mongo import *
+try: # user may be a mongodb user and not want pyg.sql
+    from pyg.sql import *
+except Exception:
+    print('pyg_sql not imported')
+try: # user may be a ms-sql user and not want pyg.mongo
+    from pyg.mongo import *
+except Exception:
+    print('pyg_mongo not imported')
 from pyg.cells import *
 from pyg.timeseries import *
