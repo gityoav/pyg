dcc2146152cebcd64a83a6e681dc8766e8d4cf93
gityoav <30290427+gityoav@users.noreply.github.com>
2022-07-18 23:05:23 +0100

    pygpyg

--- src/pyg/__init__.py
+++ src/pyg/__init__.py
@@ -1,16 +1,42 @@
 import pandas as pd; import numpy as np
 from functools import partial, reduce
-from pyg.npy import *
-from pyg.base import *
-from pyg.bond import * 
-from pyg.encoders import * 
-try: # user may be a mongodb user and not want pyg.sql
+
+try: 
+    from pyg.npy import *
+except Exception:
+    print('pyg_npy not imported')
+
+try:
+    from pyg.base import *
+except Exception:
+    print('pyg_base not imported')
+
+try:
+    from pyg.bond import * 
+except Exception:
+    print('pyg_bond not imported')
+
+try:
+    from pyg.encoders import * 
+except Exception:
+    print('pyg_encoders not imported')
+
+try: 
     from pyg.sql import *
 except Exception:
     print('pyg_sql not imported')
-try: # user may be a ms-sql user and not want pyg.mongo
+
+try: 
     from pyg.mongo import *
 except Exception:
     print('pyg_mongo not imported')
-from pyg.cells import *
-from pyg.timeseries import *
+
+try: 
+    from pyg.cells import *
+except Exception:
+    print('pyg_cell not imported')
+
+try: 
+    from pyg.timeseries import *
+except Exception:
+    print('pyg_timeseries not imported')
